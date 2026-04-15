ebec0a86e385530e234b50fc5331fc871e64ca4c
yuiii <yuiii@C010422915.zipangu.cac.co.jp>
2026-04-15 10:25:55 +0900

    lesson01修正

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,62 @@
 
 package lesson01.challenge06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+
+		int cake1 = 30;
+		int cake2 = 30;
+		int cake3 = 30;
+
+		System.out.println("シトロン    　\\250・・・残り" + cake1 + "個");
+		System.out.println("ショコラ　    \\280・・・残り" + cake2 + "個");
+		System.out.println("ピスターシュ　\\320・・・残り" + cake3 + "個");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("\nそれぞれ何個ずつ買いますか？（最大30個まで）\n");
+
+		System.out.println("シトロン　>");
+		String citronBuyCountStr = reader.readLine();
+		int citronBuyCount = Integer.parseInt(citronBuyCountStr);
+
+		System.out.println("ショコラ　>");
+		String chocolateBuyCountStr = reader.readLine();
+		int chocolateBuyCount = Integer.parseInt(chocolateBuyCountStr);
+
+		System.out.println("ピスターシュ　>");
+		String pistaBuyCountStr = reader.readLine();
+		int pistaBuyCount = Integer.parseInt(pistaBuyCountStr);
+
+		System.out.println("\nシトロン　" + citronBuyCount + "個");
+		System.out.println("ショコラ　" + chocolateBuyCount + "個");
+		System.out.println("ショコラ　" + pistaBuyCount + "個");
+
+		int toalBuyCount = citronBuyCount + chocolateBuyCount + pistaBuyCount;
+		int totalprice = 250 * citronBuyCount + 280 * chocolateBuyCount + 320 * pistaBuyCount;
+
+		System.out.println("\n合計個数　" + toalBuyCount + "個");
+		System.out.println("合計金額　" + totalprice + "円");
+		System.out.println("\nをお買い上げですね。");
+		System.out.println("承りました。");
+
+		cake1 = cake1 - citronBuyCount;
+		cake2 = cake2 - chocolateBuyCount;
+		cake3 = cake3 - pistaBuyCount;
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+		System.out.println("シトロン　\\250・・・残り" + cake1 + "個");
+		System.out.println("ショコラ　\\280・・・残り" + cake2 + "個");
+		System.out.println("ピスターシュ　\\320・・・残り" + cake3 + "個");
+
+	}
 
 }
